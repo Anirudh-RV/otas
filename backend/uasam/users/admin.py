66a7a67a32f35accab3054144b4efe983e58dd5e
Yeefong Wu <yeefongwu@gmail.com>
2026-02-12 22:07:18 -0800

    Update user admin configuration

--- backend/uasam/users/admin.py
+++ backend/uasam/users/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import User
+from .models import User 
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "middle_name",
+        "last_name",
+        "email",
+        "password",
+        "created_at",
+        "updated_at",
+    )
