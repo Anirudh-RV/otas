0543bec63d47baefc06d1171809320c11204e306
Akshantula Neha <akshantulaneha@AkshantasLaptop.lan>
2026-02-21 18:04:44 -0800

    OTAS 26 & OTAS 27

--- backend/uasam/agents/urls.py
+++ backend/uasam/agents/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import CreateAgentSessionViewV1, AgentCreateView
+from .views import CreateAgentSessionViewV1, AgentCreateView, AgentListView, AgentSessionListView
 
 urlpatterns = [
     path('v1/create/', csrf_exempt(AgentCreateView.as_view()), name='agent-create'),
     path('v1/session/create/', csrf_exempt(CreateAgentSessionViewV1.as_view()), name='agent-session-create'),
+    path('v1/list/', csrf_exempt(AgentListView.as_view()), name='agent-list'),
+    path('v1/sessions/list/', csrf_exempt(AgentSessionListView.as_view()), name='agent-session-list'),
 ]
